fix computation of circles intersection

robotics_toolbox/utils/geometry_utils.py
@@ -29,12 +29,36 @@ def circle_circle_intersection(
     Returns empty array if there is no solution, two solutions otherwise. If there
     are infinite number of solutions, select two (almost) randomly.
     """
-    ca = Point(c0).buffer(r0, quad_segs=64).boundary
-    cb = Point(c1).buffer(r1, quad_segs=64).boundary
-    ints = ca.intersection(cb)
-    if ints.is_empty:
+    c0 = np.asarray(c0)
+    c1 = np.asarray(c1)
+    d = np.linalg.norm(c1 - c0)
+    if r0 + r1 < d < np.abs(r0 - r1):
         return []
-    return [g.coords[0] for g in islice(ints.geoms, 2)]
+
+    if np.isclose(d, 0) and np.isclose(r0, r1):
+        return [
+            c0 + [r0 * np.cos(a), r0 * np.sin(a)]
+            for a in np.random.uniform(-np.pi, np.pi, 2)
+        ]
+    a = (r0**2 - r1**2 + d**2) / (2 * d)
+    h = np.sqrt(r0**2 - a**2)
+    x0, y0 = c0
+    x1, y1 = c1
+    x2 = x0 + a * (x1 - x0) / d
+    y2 = y0 + a * (y1 - y0) / d
+    x3 = x2 + h * (y1 - y0) / d
+    y3 = y2 - h * (x1 - x0) / d
+    x4 = x2 - h * (y1 - y0) / d
+    y4 = y2 + h * (x1 - x0) / d
+
+    return [np.array([x3, y3]), np.array([x4, y4])]
+
+    # ca = Point(c0).buffer(r0, quad_segs=10000).boundary
+    # cb = Point(c1).buffer(r1, quad_segs=10000).boundary
+    # ints = ca.intersection(cb)
+    # if ints.is_empty:
+    #     return []
+    # return [g.coords[0] for g in islice(ints.geoms, 2)]
 
 
 def circle_line_intersection(c0: ArrayLike, r0: float, a1: ArrayLike, b1: ArrayLike):