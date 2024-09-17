a9130404f55aedc60254d3bf17d853ebe608120b
Vladimir Petrik <vladimir.petrik@cvut.cz>
2024-09-17 15:29:30 +0200

    se3 independent

--- tests/hw01/mandatory/test_se3.py
+++ tests/hw01/mandatory/test_se3.py
@@ -6,6 +6,7 @@
 #
 import unittest
 import numpy as np
+from numpy.testing import assert_allclose
 import inspect
 import pinocchio as pin
 
@@ -13,15 +14,11 @@ from robotics_toolbox.core import SE3, SO3
 
 
 class TestSE3(unittest.TestCase):
-    def test_initialization(self):
-        self.assertEqual(SE3(), SE3(np.zeros(3), SO3()))
+    """SE3 class depends on SO3, implement SO3 first."""
 
-    def test_only_rotation_and_translation_variables(self):
-        t = SE3()
-        all_vars = list(vars(t).keys())
-        self.assertEqual(len(all_vars), 2)
-        self.assertTrue("translation" in all_vars)
-        self.assertTrue("rotation" in all_vars)
+    def test_initialization(self):
+        """Constructor implemented, this test pass by default."""
+        self.assertEqual(SE3(), SE3(np.zeros(3), SO3()))
 
     def test_act(self):
         np.random.seed(0)
@@ -34,19 +31,15 @@ class TestSE3(unittest.TestCase):
             ref_v_ = ref_t.act(v)
             self.assertTrue(np.allclose(v_, ref_v_))
 
-    def test_imported_modules(self):
-        """Test that you are not using pinocchio inside your implementation."""
-        with open(inspect.getfile(SE3)) as f:
-            self.assertTrue("pinocchio" not in f.read())
-
     def test_inverse(self):
         np.random.seed(0)
         for _ in range(100):
             a = np.random.uniform(-2 * np.pi, 2 * np.pi, size=3)
             t = np.random.uniform(-10, 10, size=3)
             r = SO3(pin.exp(a))
-            self.assertEqual(SE3(t, r) * SE3(t, r).inverse(), SE3())
-            self.assertEqual(SE3(t, r).inverse() * SE3(t, r), SE3())
+            pose = SE3(t, r)
+            exp = pin.SE3(pose.homogeneous()).inverse()
+            assert_allclose(pose.inverse().homogeneous(), exp.homogeneous)
 
     def test_composition(self):
         np.random.seed(0)
@@ -67,6 +60,18 @@ class TestSE3(unittest.TestCase):
 
             self.assertTrue(np.allclose(m[:3, :3], tc.rotation.rot))
             self.assertTrue(np.allclose(m[:3, 3], tc.translation))
+
+    def test_only_rotation_and_translation_variables(self):
+        t = SE3()
+        all_vars = list(vars(t).keys())
+        self.assertEqual(len(all_vars), 2)
+        self.assertTrue("translation" in all_vars)
+        self.assertTrue("rotation" in all_vars)
+
+    def test_imported_modules(self):
+        """Test that you are not using pinocchio inside your implementation."""
+        with open(inspect.getfile(SE3)) as f:
+            self.assertTrue("pinocchio" not in f.read())
 
 
 if __name__ == "__main__":
