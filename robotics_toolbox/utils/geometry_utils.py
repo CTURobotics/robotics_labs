add tests and fix circle line bug with sign

robotics_toolbox/utils/geometry_utils.py
@@ -8,7 +8,6 @@ from __future__ import annotations
 
 import numpy as np
 from numpy.typing import ArrayLike
-from shapely.geometry import Point
 
 
 def nullspace(A, atol=1e-13, rtol=0.0):
@@ -75,7 +74,8 @@ def circle_line_intersection(
     else:
         sols = []
         for pm in [-1, 1]:
-            x = (d * dy + pm * np.sign(dy) * dx * np.sqrt(discriminant)) / (dr**2)
+            sgn_dy = 1 if dy > 0 else -1
+            x = (d * dy + pm * sgn_dy * dx * np.sqrt(discriminant)) / (dr**2)
             y = (-d * dx + pm * np.abs(dy) * np.sqrt(discriminant)) / (dr**2)
             sols.append(np.array([x, y]) + c)
         return sols

tests/test_geometry_utils.py
@@ -6,7 +6,7 @@
 #
 import unittest
 import numpy as np
-from robotics_toolbox.utils import circle_circle_intersection
+from robotics_toolbox.utils import circle_circle_intersection, circle_line_intersection
 
 
 class TestGeometryUtils(unittest.TestCase):
@@ -34,6 +34,29 @@ class TestGeometryUtils(unittest.TestCase):
         a, b = ints
         self.assertAlmostEqual(np.linalg.norm(a), 1.0)
         self.assertAlmostEqual(np.linalg.norm(b), 1.0)
+
+    def test_circ_line_intersection(self):
+        ints = circle_line_intersection([0, 0], 1, [-5, 0], [5, 0])
+        self.assertEqual(len(ints), 2)
+        print(ints)
+        (ax, ay), (bx, by) = ints
+        if ax > bx:
+            ax, bx = bx, ax
+            ay, by = by, ay
+        self.assertAlmostEqual(ax, -1.0)
+        self.assertAlmostEqual(ay, 0.0)
+        self.assertAlmostEqual(bx, 1.0)
+        self.assertAlmostEqual(by, 0.0)
+
+        ints = circle_line_intersection([0, 0], 2, [0, 5], [0, -5])
+        self.assertEqual(len(ints), 2)
+        (ax, ay), (bx, by) = ints
+        if ay > by:
+            ay, by = by, ay
+        self.assertAlmostEqual(ax, 0.0)
+        self.assertAlmostEqual(ay, -2.0)
+        self.assertAlmostEqual(bx, 0.0)
+        self.assertAlmostEqual(by, 2.0)
 
 
 if __name__ == "__main__":