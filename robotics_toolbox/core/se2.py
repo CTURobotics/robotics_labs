a6eda24b30d7af768daac5b359e6acf2bb21f004
Vladimir Petrik <vladimir.petrik@cvut.cz>
2024-09-17 15:01:35 +0200

    decomposition of se2

--- robotics_toolbox/core/se2.py
+++ robotics_toolbox/core/se2.py
@@ -6,6 +6,8 @@
 #
 
 from __future__ import annotations
+
+import copy
 import numpy as np
 from numpy.typing import ArrayLike
 
@@ -18,26 +20,32 @@ class SE2:
     def __init__(
         self, translation: ArrayLike | None = None, rotation: SO2 | float | None = None
     ) -> None:
-        """Crete an SE2 transformation. Identity is the default."""
+        """Crete an SE2 transformation. Identity is the default. The se3 instance
+        is represented by translation and rotation, where rotation is SO2 instance.
+
+        Attributes:
+        :param translation: 2D vector representing translation
+        :param rotation: SO2 rotation or angle in radians that is converted to SO2
+        """
         super().__init__()
         self.translation = (
             np.asarray(translation) if translation is not None else np.zeros(2)
         )
+        assert self.translation.shape == (2,)
         if isinstance(rotation, SO2):
             self.rotation = rotation
         elif isinstance(rotation, float):
             self.rotation = SO2(rotation)
         else:
             self.rotation = SO2()
-        assert self.translation.shape == (2,)
 
     def __mul__(self, other: SE2) -> SE2:
         """Compose two transformation, i.e., self * other"""
         # todo: HW01: implement composition of two transformation.
-        pass
+        return SE2()
 
     def inverse(self) -> SE2:
-        """Compute inverse of the transformation"""
+        """Compute inverse of the transformation. Do not use np.linalg.inv."""
         # todo: HW1 implement inverse
         return SE2()
 
@@ -50,8 +58,15 @@ class SE2:
 
     def set_from(self, other: SE2):
         """Copy the properties into current instance."""
-        self.translation = other.translation
-        self.rotation = other.rotation
+        self.translation = copy.deepcopy(other.translation)
+        self.rotation = copy.deepcopy(other.rotation)
+
+    def homogeneous(self) -> np.ndarray:
+        """Return homogeneous transformation matrix."""
+        h = np.eye(3)
+        h[:2, :2] = self.rotation.rot
+        h[:2, 2] = self.translation
+        return h
 
     def __eq__(self, other: SE2) -> bool:
         """Returns true if two transformations are almost equal."""
--- tests/hw01/mandatory/test_se2.py
+++ tests/hw01/mandatory/test_se2.py
@@ -13,17 +13,16 @@ from robotics_toolbox.core import SE2, SO2
 
 
 class TestSE2(unittest.TestCase):
-    def test_initialization(self):
-        self.assertEqual(SE2(), SE2(np.zeros(2), SO2(angle=0.0)))
+    """SE2 testing depends on SO2 implementation, there it is needed to implement SO2
+    first."""
 
-    def test_only_rotation_and_translation_variables(self):
-        t = SE2()
-        all_vars = list(vars(t).keys())
-        self.assertEqual(len(all_vars), 2)
-        self.assertTrue("translation" in all_vars)
-        self.assertTrue("rotation" in all_vars)
+    def test_initialization(self):
+        """Test constructor. No implementation needed."""
+        self.assertEqual(SE2(), SE2(np.zeros(2), SO2(angle=0.0)))
+        self.assertEqual(SE2(), SE2(np.zeros(2), 0.0))
 
     def test_act(self):
+        """Required to implement act to pass."""
         np.random.seed(0)
         for _ in range(100):
             v = np.random.rand(2)
@@ -34,18 +33,14 @@ class TestSE2(unittest.TestCase):
             ref_v_ = ref_t.act(np.append(v, 0))
             self.assertTrue(np.allclose(v_, ref_v_[:2]))
 
-    def test_imported_modules(self):
-        """Test that you are not using pinocchio inside your implementation."""
-        with open(inspect.getfile(SE2)) as f:
-            self.assertTrue("pinocchio" not in f.read())
-
     def test_inverse(self):
         np.random.seed(0)
         for _ in range(100):
             a = np.random.uniform(-2 * np.pi, 2 * np.pi)
             t = np.random.uniform(-10, 10, size=2)
-            self.assertEqual(SE2(t, SO2(a)) * SE2(t, SO2(a)).inverse(), SE2())
-            self.assertEqual(SE2(t, SO2(a)).inverse() * SE2(t, SO2(a)), SE2())
+            pose = SE2(t, a)
+            m = pose.inverse().homogeneous()
+            self.assertTrue(np.allclose(np.linalg.inv(pose.homogeneous()), m))
 
     def test_composition(self):
         np.random.seed(0)
@@ -65,6 +60,21 @@ class TestSE2(unittest.TestCase):
 
             self.assertTrue(np.allclose(m[:2, :2], tc.rotation.rot))
             self.assertTrue(np.allclose(m[:2, 3], tc.translation))
+
+    def test_imported_modules(self):
+        """Test that you are not using pinocchio inside your implementation. Passing by
+        default."""
+        with open(inspect.getfile(SE2)) as f:
+            self.assertTrue("pinocchio" not in f.read())
+
+    def test_only_rotation_and_translation_variables(self):
+        """Test that SE2 has only rotation and translation variables. Passing by
+        default."""
+        t = SE2()
+        all_vars = list(vars(t).keys())
+        self.assertEqual(len(all_vars), 2)
+        self.assertTrue("translation" in all_vars)
+        self.assertTrue("rotation" in all_vars)
 
 
 if __name__ == "__main__":
