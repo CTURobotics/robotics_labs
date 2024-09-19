8e49abe82539fdcc14412e1ebf760557af5db428
Vladimir Petrik <vladimir.petrik@cvut.cz>
2024-09-19 14:23:57 +0200

    added readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+project = 'Robotics Toolbox'
+copyright = 'CTU'
+author = 'Vladimir Petrik'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # google and numpy docstrings
+]
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+autodoc_typehints = "description"
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+Robotics Toolbox
+==============================
+
+To be done.
+
+.. automodule:: robotics_toolbox
+   :members:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
