9cd88b92e2fb9ce3886eae222d0b8cd0eb393f4a
Vladimir Petrik <vladimir.petrik@cvut.cz>
2024-09-23 15:59:02 +0200

    update sampling random conf. and link to hw

--- tests/hw02/mandatory/test_spatial_urdf.py
+++ tests/hw02/mandatory/test_spatial_urdf.py
@@ -6,10 +6,11 @@
 #
 import unittest
 from pathlib import Path
-
 import pickle
+import inspect
 import numpy as np
 import pinocchio as pin
+from robotics_toolbox.robots import PlanarManipulator
 
 
 def check_if_identity(pose1: pin.SE3, pose2: pin.SE3, error: float = 0.001) -> bool:
@@ -21,23 +22,22 @@ def check_if_identity(pose1: pin.SE3, pose2: pin.SE3, error: float = 0.001) -> b
 class TestSpatialURDF(unittest.TestCase):
     def test_poses_of_links_frames(self):
         """test whether the poses of links are similar to the reference ones"""
+        np.random.seed(0)
         # making pinocchio model for testing
-        path = Path.joinpath(
-            Path(__file__).parents[3], "src/robotics_toolbox/robots/robot_hw.urdf"
-        )
+        path = Path(inspect.getfile(PlanarManipulator)).parent / "robot_hw.urdf"
         student_mod, _, _ = pin.buildModelsFromUrdf(str(path))
         student_data = student_mod.createData()
 
         # load model which will be used to check the solution
-        path = Path.joinpath(Path(__file__).parents[0], "pin_mod_pin_data.pickle")
+        path = Path(__file__).parent / "pin_mod_pin_data.pickle"
         with open(path, "rb") as file:
             data = pickle.load(file)
         pin_mod = data["mod"]
         pin_data = data["data"]
 
-        # check 10 rand configurations with corespondence model
-        for i in range(10):
-            c = np.random.random(4) * np.pi - np.random.random(4) * np.pi
+        # check 100 rand configurations with corespondence model
+        for i in range(100):
+            c = pin.randomConfiguration(pin_mod)
             pin.forwardKinematics(student_mod, student_data, c)
             pin.updateFramePlacements(student_mod, student_data)
 
@@ -45,7 +45,9 @@ class TestSpatialURDF(unittest.TestCase):
             pin.updateFramePlacements(pin_mod, pin_data)
             self.assertTrue(
                 check_if_identity(pin_data.oMf[-1], student_data.oMf[-1]),
-                msg=f"Configuration {i} failed. \n frame {i} should be \n {pin_data.oMf[-1].homogeneous} \n but is \n {student_data.oMf[-1].homogeneous}",
+                msg=f"Configuration {i} failed. \n frame {i} should be \n "
+                f"{pin_data.oMf[-1].homogeneous} \n but is \n "
+                f"{student_data.oMf[-1].homogeneous}",
             )
 
 
